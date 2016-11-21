f4ae884e700d1e8a770e6faf33a90b060b366f64
liuzhiyi1992 <55137840@qq.com>
2016-11-21 11:37:46 +0800

    Initial Commit

---
+++ ZYCarouselScroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCarouselScroller
+//
+//  Created by zhiyi on 16/11/21.
+//  Copyright © 2016年 zhiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYCarouselScroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYCarouselScroller
+//
+//  Created by zhiyi on 16/11/21.
+//  Copyright © 2016年 zhiyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
