7df36fac1f2bff80973ddf64f8e53e4e808023d3
liuzhiyi1992 <55137840@qq.com>
2016-11-22 12:06:41 +0800

    完善 海豚村样式 布局

--- ZYCarouselScroller/CustomCell/DolphinGoodsCollectionViewCell.m
+++ ZYCarouselScroller/CustomCell/DolphinGoodsCollectionViewCell.m
@@ -15,9 +15,7 @@
 @property (copy, nonatomic) NSArray *imageViewList;
 @property (copy, nonatomic) NSArray *titleLabelList;
 @property (copy, nonatomic) NSArray *priceLabelList;
-
-@property (copy, nonatomic) NSArray *goodsImageUrlList;
-@property (copy, nonatomic) NSArray *bigImageUrlList;
+@property (strong, nonatomic) NSLayoutConstraint *mainImageViewHeightConstraint;
 @end
 
 @implementation DolphinGoodsCollectionViewCell
@@ -28,33 +26,44 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        _goodsImageUrlList = [self generateGoodsImageUrlList];
-        _bigImageUrlList = [self generateBigImageUrlList];
         [self setupCell];
     }
     return self;
 }
 
 - (void)updateCellWithDataDict:(NSDictionary *)dataDict {
-    
+    //bigImage
+    NSString *bigImageUrlString = [dataDict objectForKey:kBIG_IMAGE_URL_STRING];
+    if (bigImageUrlString.length > 0) {
+        [_mainImageView sd_setImageWithURL:[NSURL URLWithString:bigImageUrlString]];
+    }
+    //goodsImage
+    NSArray *goodsImageUrlList = [dataDict objectForKey:kGOODS_IMAGE_URL_STRING_LIST];
+    if (goodsImageUrlList) {
+        for (int i = 0; i < goodsImageUrlList.count; i ++) {
+            NSString *imageUrlString = goodsImageUrlList[i];
+            if (i < _imageViewList.count) {
+                [(UIImageView *)_imageViewList[i] sd_setImageWithURL:[NSURL URLWithString:imageUrlString]];
+            }
+        }
+    }
 }
 
 - (void)setupCell {
     self.mainImageView = [[UIImageView alloc] init];
-    [_mainImageView sd_setImageWithURL:[self randomBigImageUrl]];
+    [self observeBackgroundImageChange];
+    [_mainImageView setBackgroundColor:[UIColor grayColor]];
     [self.contentView addSubview:_mainImageView];
     
     NSMutableArray *imageArray = [NSMutableArray array];
     UIImageView *firstImageView = [[UIImageView alloc] init];
-    [firstImageView sd_setImageWithURL:[self randomGoodsImageUrl]];
     [self.contentView addSubview:firstImageView];
     [imageArray addObject:firstImageView];
     UIImageView *secondImageView = [[UIImageView alloc] init];
-    [secondImageView sd_setImageWithURL:[self randomGoodsImageUrl]];
+    [secondImageView setBackgroundColor:[UIColor greenColor]];
     [self.contentView addSubview:secondImageView];
     [imageArray addObject:secondImageView];
     UIImageView *thirdImageView = [[UIImageView alloc] init];
-    [thirdImageView sd_setImageWithURL:[self randomGoodsImageUrl]];
     [self.contentView addSubview:thirdImageView];
     [imageArray addObject:thirdImageView];
     self.imageViewList = [imageArray copy];
@@ -93,7 +102,7 @@
     
     __weak __typeof(&*self)weakSelf = self;
     [_mainImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.equalTo(weakSelf).multipliedBy(0.4);
+        make.height.equalTo(weakSelf).multipliedBy(0.4).priority(900);//具体看图片比例
         make.top.equalTo(weakSelf.contentView);
         make.leading.and.trailing.equalTo(weakSelf.contentView);
     }];
@@ -144,45 +153,35 @@
     }];
 }
 
-- (NSArray *)generateGoodsImageUrlList {
-    return @[
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajphus60010003x.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajpcos10010038_1.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/b/l/blmnzspl30020018x2.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/r/e/red-seal-molasses-500g2_1.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/s/w/swiauspl30020011x3.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbagbspl30010016x2_1_1.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/c/h/chiusspl30010002_1.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/r/s/rslauspl30020002x3.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/w/l/wldusbbya0030003.jpg?imageView2/0/w/232/h/232",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010003.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010058x6_1.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010062__1.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajpcos10010023.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbagbcos10010005x2.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/o/v/ovdauspl30010008x2.jpg?imageView2/0/w/200/h/200",
-             @"http://assets.haituncun.com/media/catalog/product/f/b/fbadespl30020004x2.jpg?imageView2/0/w/200/h/200"
-             ];
+- (void)observeBackgroundImageChange {
+    [_mainImageView addObserver:self forKeyPath:@"image" options:NSKeyValueObservingOptionNew context:nil];
 }
 
-- (NSArray *)generateBigImageUrlList {
-    return @[
-             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6c3f5e0ca91367d9d35f2afc7db7f1ee.jpg",
-             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/2299e0ee63407d8d3d454c3e99acea61.jpg",
-             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6566fb1cfe55d5f5f39d11097156efa7.jpg",
-             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6855eb4cc29edc52c7f6961d21e1ca44.jpg",
-             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/9574777713406cae924043fd63452b3a.jpg"
-             ];
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if ([keyPath isEqualToString:@"image"]) {
+        UIImage *newImage = change[NSKeyValueChangeNewKey];
+        if ([newImage isMemberOfClass:[NSNull class]]) {
+            return;
+        }
+        [self adaptiveBackgroundImageRatioWithImage:newImage];
+    }
 }
 
-- (NSURL *)randomGoodsImageUrl {
-    NSString *urlString = [_goodsImageUrlList objectAtIndex:arc4random()%_goodsImageUrlList.count];
-    return [NSURL URLWithString:urlString];
-}
-
-- (NSURL *)randomBigImageUrl {
-    NSString *urlString = [_bigImageUrlList objectAtIndex:arc4random()%_bigImageUrlList.count];
-    return [NSURL URLWithString:urlString];
+/**
+ BackgroundImage比例适配
+ */
+- (void)adaptiveBackgroundImageRatioWithImage:(UIImage *)image {
+    if (0 == image.size.width || 0 == image.size.height) {
+        return;
+    }
+    CGFloat fittingHeight = self.frame.size.width / image.size.width * image.size.height;
+    if (!_mainImageViewHeightConstraint) {
+        self.mainImageViewHeightConstraint = [NSLayoutConstraint constraintWithItem:_mainImageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.f constant:fittingHeight];
+        [_mainImageViewHeightConstraint setPriority:UILayoutPriorityRequired];
+        [self.contentView addConstraint:_mainImageViewHeightConstraint];
+    } else {
+        _mainImageViewHeightConstraint.constant = fittingHeight;
+    }
 }
 @end
 
--- ZYCarouselScroller/ViewController.m
+++ ZYCarouselScroller/ViewController.m
@@ -50,10 +50,70 @@
                       updateCarouselCellBlock:^(UICollectionViewCell *cell, NSDictionary *dataDict) {
                           [(DolphinGoodsCollectionViewCell *)cell updateCellWithDataDict:dataDict];
                       }];
-//    zyCarouselScrollerSecond.dataList = @[];
+    zyCarouselScrollerSecond.dataList = [self generateDolphinDataList];
+    zyCarouselScrollerSecond.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1];
     [self.view addSubview:zyCarouselScrollerSecond];
 }
 
+- (NSArray *)generateDolphinGoodsImageUrlList {
+    return @[
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajphus60010003x.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajpcos10010038_1.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/b/l/blmnzspl30020018x2.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/r/e/red-seal-molasses-500g2_1.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/s/w/swiauspl30020011x3.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbagbspl30010016x2_1_1.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/c/h/chiusspl30010002_1.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/r/s/rslauspl30020002x3.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/w/l/wldusbbya0030003.jpg?imageView2/0/w/232/h/232",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010003.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010058x6_1.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbakrcos10010062__1.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbajpcos10010023.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbagbcos10010005x2.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/o/v/ovdauspl30010008x2.jpg?imageView2/0/w/200/h/200",
+             @"http://assets.haituncun.com/media/catalog/product/f/b/fbadespl30020004x2.jpg?imageView2/0/w/200/h/200"
+             ];
+}
+
+- (NSArray *)generateDolphinBigImageUrlList {
+    return @[
+             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6c3f5e0ca91367d9d35f2afc7db7f1ee.jpg",
+             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/2299e0ee63407d8d3d454c3e99acea61.jpg",
+             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6566fb1cfe55d5f5f39d11097156efa7.jpg",
+             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/6855eb4cc29edc52c7f6961d21e1ca44.jpg",
+             @"http://app-img.haituncun.com/uploads/images/recommend/bannner/9574777713406cae924043fd63452b3a.jpg"
+             ];
+}
+
+
+/**
+ Generate 3/Gruop GoodsImage random
+ */
+- (NSArray *)randomThreeGoodsImageUrlGruop {
+    NSArray *goodsImageList = [self generateDolphinGoodsImageUrlList];
+    NSMutableArray *tmpArray = [NSMutableArray array];
+    for (int i = 0; i < 3; i ++) {
+        NSString *randomImageUrlString = [goodsImageList objectAtIndex:arc4random()%goodsImageList.count];
+        [tmpArray addObject:randomImageUrlString];
+    }
+    return [tmpArray copy];
+}
+
+/**
+ Generate haituncun.com style cell dataList
+ a big image & three goodsImage a Gruop
+ */
+- (NSArray *)generateDolphinDataList {
+    NSArray *bigImageList = [self generateDolphinBigImageUrlList];
+    NSMutableArray *tmpDataList = [NSMutableArray array];
+    for (NSString *bigImageUrlString in bigImageList) {
+        NSDictionary *dataDict = @{kBIG_IMAGE_URL_STRING:bigImageUrlString,
+                            kGOODS_IMAGE_URL_STRING_LIST:[self randomThreeGoodsImageUrlGruop]};
+        [tmpDataList addObject:dataDict];
+    }
+    return [tmpDataList copy];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
